2c489a4786cc2a16e7a6cbbc1d6ed7923abf40ab
Dhritiman <Dhritiman@Dhritiman.local>
2015-05-25 15:19:54 +0530

    Initial Commit

---
+++ AllInOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AllInOne
+//
+//  Created by Dhritiman on 25/05/15.
+//  Copyright (c) 2015 CapitalNumbers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AllInOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllInOne
+//
+//  Created by Dhritiman on 25/05/15.
+//  Copyright (c) 2015 CapitalNumbers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
